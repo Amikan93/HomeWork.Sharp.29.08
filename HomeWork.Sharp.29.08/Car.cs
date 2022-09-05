439be7920fae452f60be131ffc722a96c4f7b04d
Amikan <Amikan@DESKTOP-FQHEF60>
2022-09-05 17:54:35 +0600

    Добавьте файлы проекта.

---
+++ HomeWork.Sharp.29.08/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork.Sharp._29._08
+{
+    public class Car
+    {
+        public string carBrand;
+        public double carPrice;
+        public string bodyType;
+        public string carColor;
+        public int carPower;
+
+        public void Print()
+        {
+            Console.WriteLine("Информация о машине:");
+            Console.WriteLine($"Марка: {carBrand}");
+            Console.WriteLine($"Цена: {carPrice}");
+            Console.WriteLine($"Тип: {bodyType}"); 
+            Console.WriteLine($"Мощность двигателя: {carPower}");
+        }
+
+        public string GetFullCar()
+        {
+            return $"{carBrand} {carColor} {carPrice}";
+        }
+    }
+}
---
+++ HomeWork.Sharp.29.08/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork.Sharp._29._08
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //    var firstCar = GetCar();
+            //    firstCar.Print();
+            //}
+
+            var firstCar = new Car();
+            firstCar.Print();
+        }
+    }
+}
